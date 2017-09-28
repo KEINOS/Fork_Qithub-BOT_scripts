d453e5e0ac40b7209955b090e65a790e17fc8556
KEINOS <github@keinos.com>
2017-09-29 01:12:47 +0900

    #10 の最初の要件「Hello world!」を返すだけのスクリプト
    同時に #7 に準拠したディレクトリ構成も作成

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Front to the Qithub application.
+ *
+ * This file doesn't do anything, yet. Though it shows the "Hello world!"
+ * to see if this script's moving properly.
+ *
+ * This application is a BOT that operate of, by and for the Qiita users.
+ *
+ * Such as :
+ *     Collaborate in writing Qiita articles via GitHub repository.
+ *     Collaborate in customize Qiitadon Toot Bot function.
+ *     Etc.
+ * See more at :
+ *     https://github.com/Qithub-BOT/
+ *
+ */
+ 
+ // KISS から始まるストーリー
+ echo 'Hello world!';
