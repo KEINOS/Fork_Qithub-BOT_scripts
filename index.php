a4d18f0a59e4e703593706b938d612c7094d4a7f
KEINOS <github@keinos.com>
2017-10-02 17:05:25 +0900

    コンフリクト修正のため一旦オリジナルを上書き
    ヘッダーが大きく修正されすぎたようです。

--- index.php
+++ index.php
@@ -2,11 +2,19 @@
 /**
  * Front to the Qithub application.
  *
- * Call this script from the browser, WebHook, cron, etc. to execute
- * the BOT routine.
+ * This file doesn't do anything, yet. Though it shows the "Hello world!"
+ * to see if this script's moving properly.
  *
- * See more at GitHub:
- *     https://github.com/Qithub-BOT/sctipts
+ * This application is a BOT that operate of, by and for the Qiita users.
+ *
+ * Such as :
+ *     Collaborate in writing Qiita articles via GitHub repository.
+ *     Collaborate in customize Qiitadon Toot Bot function.
+ *     Etc.
+ * See more at :
+ *     https://github.com/Qithub-BOT/
  *
  */
-
+ 
+ // KISS から始まるストーリー
+ echo 'Hello world!';
