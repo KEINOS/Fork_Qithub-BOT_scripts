136cb90383856f6e9f98c2ca01b34aab27b14a5e
KEINOS <github@keinos.com>
2017-11-06 01:40:20 +0900

    DEV環境とデプロイ環境でのトゥート内容の切り替えを引数レベルで対応
    - コメントを追加
    - 関数を別ファイルに分割（SideCI対応）
    - PSR-2 fix

--- system/post-toot/main.php
+++ system/post-toot/main.php
@@ -17,6 +17,9 @@
  *                                は「もっと読む」内の非表示領域になります。
  *     'in_reply_to_id' => string この項目があると値のトゥートIDに対して
  *                                返信扱いになります。
+ *     'is_mode_dev'    => string 'yes' = DEV環境用の動作
+ *     'is_mode_debug'  => bool   デプロイ、DEV環境関係なくデバッグモード
+ *                                で動かしたい場合は true
  *
  * ■出力項目
  *     'result' => string 'OK','NG'
@@ -27,6 +30,8 @@
 /* =====================================================================
     Main
    ===================================================================== */
+// 関数の読み込み（Side CI 対応）
+require('functions.php.inc');
 
 // UTF-8, TIMEZONEを日本仕様に設定
 set_utf8_ja('Asia/Tokyo');
@@ -34,44 +39,65 @@ set_utf8_ja('Asia/Tokyo');
 // 標準入力を取得
 $arg = get_api_input_as_array();
 
-// 必須項目を満たしていた場合の処理
+// ■ 必須項目を満たしていた場合
+// Mastodon API 用のリクエスト・クエリを作成し、トゥート後、Mastodon API
+// からのレスポンス結果（JSON）をそのまま、 Qithub API の "Value" に代入
+// して、Qithub API フォーマットで返します。
+// ■ 必須項目をみたしていない場合
+// "result" = "NG" で Qithub API フォーマットで返します。
 if (is_requirement_complied($arg)) {
-    $toot_msg  = "#test #newapi #qithub_dev\n" . $arg['status'];
+    // DEV環境やデバッグモードの場合はトゥートにそれがわかるように付加
+    $tag_additional = '';
+    if (isset($arg['is_mode_dev']) && ('yes' == $arg['is_mode_dev'])) {
+        $tag_additional .= "\n#test #qithub_dev";
+    }
+    if (isset($arg['is_mode_debug']) && ('yes' == $arg['is_mode_debug'])) {
+        $tag_additional .= " [DEBUG MODE]";
+    }
+
+    // トゥートメッセージの作成＆API用にエスケープ
+    $toot_msg  = $arg['status'] . $tag_additional;
     $toot_msg  = rawurlencode($toot_msg);
 
+    // トゥートに必須な情報を設定
     $visibility   = $arg['visibility'];
     $access_token = $arg['access_token'];
     $domain       = $arg['domain'];
 
-    
-    // 返信の判断処理
+    // 「in_reply_to_id」がある場合は返信用のクエリを付加
     $query_additional = '';
-    if( isset($arg['in_reply_to_id']) && !empty($arg['in_reply_to_id'])){
-        $in_reply_to_id = $arg['in_reply_to_id'];
-        $query_additional = " -d 'in_reply_to_id=${in_reply_to_id}'";        
+    if (isset($arg['in_reply_to_id']) && !empty($arg['in_reply_to_id'])) {
+        $in_reply_to_id    = $arg['in_reply_to_id'];
+        $query_additional .= " -d 'in_reply_to_id=${in_reply_to_id}'";
     }
 
+    // 「spoiler_text」の指定がある場合は「CW」でトゥートのクエリを付加
+    if (isset($arg['spoiler_text']) && !empty($arg['spoiler_text'])) {
+        $spoiler_text = $arg['spoiler_text'];
+        $spoiler_text = rawurlencode($spoiler_text);
+        $query_additional .= " -d 'spoiler_text=${spoiler_text}'";
+    }
+
+    // Mastodon API 用のトゥート・リクエストのクエリ作成
     $query  = 'curl -X POST';
     $query .= " -d 'status=${toot_msg}'";
     $query .= " -d 'visibility=${visibility}'";
     $query .= $query_additional;
     $query .= " --header 'Authorization: Bearer ${access_token}'";
     $query .= " -sS https://${domain}/api/v1/statuses;";
- 
-    /*   
-     * @todo サーバが500の場合なども'OK'を返してしまうので要改善
-     */
-    $result = 'OK';
+
+    // トゥートの実行
     $result_value = `$query`;
 
+    /** @todo サーバが500の場合なども'OK'を返してしまうので要改善 */
+    $result = 'OK';
+
+    // 実行結果を Qithub API 互換で返す
     echo encode_array_to_api([
         'result' => $result,
         'value'  => $result_value,
     ]);
-
-// 必須項目が足りない場合の処理
 } else {
-
     $temp = print_r($arg, true);
 
     die(encode_array_to_api([
@@ -82,88 +108,3 @@ if (is_requirement_complied($arg)) {
 
 
 die();
-/* ---------------------------------------------------------------------
-    Functions
-   --------------------------------------------------------------------- */
-
-/**
- *  トゥートに最低限必要な項目を網羅しているかチェックします
- *
- * @param  array   $arg   API から渡された配列
- * @return boolean        必須項目を満たしていた場合は true
- */
-function is_requirement_complied($arg)
-{
-    return isset($arg['domain']) && isset($arg['access_token']) && isset($arg['status']) && isset($arg['visibility']);
-}
-
-
-/* ---------------------------------------------------------------------
-    Getter Functions
-   --------------------------------------------------------------------- */
-/**
- *  配列をJSON & URLエンコードにエンコードします
- *
- *  'system'および'plugins'は、このデータ形式で入力を受け付けます。
- *
- * @param  array  $array_value スクリプトの実行言語（'php','python'）
- * @return string              エンコード結果
- * @link https://github.com/Qithub-BOT/scripts/issues/16
- */
-function encode_array_to_api($array_value)
-{
-    $json_raw = json_encode($array_value);
-    $json_enc = urlencode($json_raw);
-
-    return $json_enc;
-}
-
-function get_api_input_as_array()
-{
-    return json_decode(get_api_input_as_json(), JSON_OBJECT_AS_ARRAY);
-}
-
-function get_api_input_as_json()
-{
-    return urldecode(get_stdin_first_arg());
-}
-
-function get_stdin_first_arg()
-{
-    // CLIからの標準入力を取得
-    global $argv;
-
-    // 引数１は必須
-    if (empty($argv[1])) {
-        print_r($argv);
-        die("Argument is empty.");
-    }
-
-    return trim($argv[1]);
-}
-
-/* ---------------------------------------------------------------------
-    Miscellaneous Functions
-   ------------------------------------------------------------------ */
-
-/**
- * Set language to Japanese UTF-8 and Time zone to Japan
- */
-function set_utf8_ja($timezone = 'Asia/Tokyo')
-{
-    if (! function_exists('mb_language')) {
-        die('This application requires mb_language.');
-    }
-
-    date_default_timezone_set($timezone);
-    setlocale(LC_ALL, 'ja_JP');
-    mb_language('ja');
-    mb_internal_encoding('UTF-8');
-    mb_http_output('UTF-8');
-    ob_start("mb_output_handler");
-}
-
-function dir_exists($path_dir)
-{
-    return is_dir($path_dir);
-}
